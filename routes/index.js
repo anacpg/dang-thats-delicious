5b9e1108fd3d47a651a2a7725b5258f8531f736d
anacpg <anacris.pg91@gmail.com>
2017-10-26 20:15:18 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController')
+
+
+// // Do work here
+// router.get('/', (req, res) => {
+// 	// const wes = { name: 'Wes', age: 100, cool: true};
+//   // res.send('Hey! It works!');
+//   // res.send(wes);
+//   // res.send(req.query.name); // localhost:7777/?name=kait&age=100
+//   // res.json(req.query);
+//   res.render('hello', {
+// 	  name: 'wes',
+// 	  dog: req.query.dog, //localhost:7777/?dog=kait
+// 	  title: 'I love food'
+//   });
+// });
+
+// router.get('/reverse/:name', (req, res) => {
+// 	const reverse = [...req.params.name].reverse().join('');
+// 	res.send(req.params.name);
+// })
+
+// router.get('/', storeController.homePage);
+router.get('/', storeController.myMiddleware, storeController.homePage);
+
+module.exports = router;
