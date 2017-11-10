99f74d4e2c8315b71f3f1480aa7fdd0954477f46
anacpg <anacris.pg91@gmail.com>
2017-11-10 17:29:33 +0100

    chapter 8

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,7 +1,7 @@
-exports.myMiddleware = (req, res, next) => {
-	req.name = '"Wes";'
-	next();
-}
+// exports.myMiddleware = (req, res, next) => {
+// 	req.name = '"Wes";'
+// 	next();
+// }
 exports.homePage = (req, res) => {
 	console.log(req.name);
 	res.render('index');
